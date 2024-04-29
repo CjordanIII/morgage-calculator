8bdeeabc279602689883384e4e9a3e8ed7f2f04e
Clarence Jordan <clarencejordan087@gmail.com>
2024-04-29 13:34:42 -0500

    morgage caculator

---
+++ README.md
@@ -0,0 +1,6 @@
+## formula for mortgage calculator
+
+[formula explained](https://www.rocketmortgage.com/learn/how-to-calculate-mortgage)
+[video of explained](https://www.google.com/search?q=morgage+rate+caculator+explained+formula&sca_esv=73a5863c59a470b2&sca_upv=1&sxsrf=ACQVn09pXovCRuWjRlCrXQsiwDSF3tvdCQ%3A1714414919205&ei=R-UvZu35C63Jp84P0uKPyA8&oq=morgage+rate+caculator+explained+formu&gs_lp=Egxnd3Mtd2l6LXNlcnAiJm1vcmdhZ2UgcmF0ZSBjYWN1bGF0b3IgZXhwbGFpbmVkIGZvcm11KgIIADIHECEYoAEYCjIHECEYoAEYCjIHECEYoAEYCkjdLlCdBVjbIXADeAGQAQCYAXagAfgNqgEEMTAuOLgBA8gBAPgBAZgCFaACug7CAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBcICBxAAGIAEGA3CAgYQABgWGB7CAgsQABiABBiGAxiKBcICCBAAGIAEGKIEwgIIEAAYogQYiQWYAwDiAwUSATEgQIgGAZAGCpIHBTExLjEwoAeRmQE&sclient=gws-wiz-serp#fpstate=ive&vld=cid:2107667e,vid:_WWVxFSHCKs,st:0)
+
+![alt text](./assets/photos/image.png)
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 const form = document.querySelector("#mortgage-form");
+let monthlyDisplay = document.querySelector("#monthlyPayment");
+let interestDisplay = document.querySelector("#interestDisplay");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,6 +13,21 @@ form.addEventListener("submit", (e) => {
 });
 
 function calculateLoan({ principal, interest, loanLength }) {
-  const monthlyPayment = principal * (interest / loanLength);
-  console.log(monthlyPayment);
+  const monthlyInterest = interest / 12;
+  const numberOfPayments = loanLength * 12;
+
+  const monthlyPayment =
+    (principal *
+      (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))) /
+    (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+  const totalPayment = monthlyPayment * numberOfPayments;
+  const totalInterestPaid = totalPayment - principal;
+
+  monthlyDisplay.textContent = `Your montthly payment ${monthlyPayment.toFixed(
+    2
+  )}`;
+  interestDisplay.textContent = `Your total intrest payment ${totalInterestPaid.toFixed(
+    2
+  )}`;
 }
