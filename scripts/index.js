5131a46f371c8fb497926d0741fc366d676ad09e
Clarence Jordan <clarencejordan087@gmail.com>
2024-04-29 12:20:11 -0500

    updated pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log("hellow world");
+const form = document.querySelector("#mortgage-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const object = {
+    principal: parseFloat(e.target.principal.value),
+    interest: parseFloat(e.target.interest.value) / 100,
+    loanLength: parseFloat(e.target.loanLength.value),
+  };
+  calculateLoan(object);
+});
+
+function calculateLoan({ principal, interest, loanLength }) {
+  const monthlyPayment = principal * (interest / loanLength);
+  console.log(monthlyPayment);
+}
